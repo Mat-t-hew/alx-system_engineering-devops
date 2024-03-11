37add28982a3355a5326734f22afa955adf84c34
Matthew <122832898+Mat-t-hew@users.noreply.github.com>
2024-03-11 21:03:09 +0200

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,36 +1,15 @@
 #!/usr/bin/python3
-"""
-Function that queries the Reddit API and returns the number of subscribers
-for a given subreddit.
-"""
+"""Function to query subscribers on a given Reddit subreddit."""
 import requests
 
-
 def number_of_subscribers(subreddit):
-    """
-    Initialize and query the Reddit API to get the number of subscribers
-    for the given subreddit.
-
-    Args:
-        subreddit: A string representing the subreddit name.
-
-    Returns:
-        The number of subscribers if successful, otherwise 0.
-    """
-    if not isinstance(subreddit, str):
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "Python/1.0(Alx Project)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
-
-    url_api = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {'User-Agent': 'Python/1.0(Alx Project)'}
-    response = requests.get(url_api, headers=headers)
-
-    if response.status_code != 200:
-        return 0
-
-    data = response.json().get("data", {})
-    subscribers = data.get("subscribers", 0)
-    return subscribers
-
-# Example usage:
-subreddit = "learnpython"
-subscribers = number_of_subscribers(subreddit)
+    results = response.json().get("data")
+    return results.get("subscribers")
