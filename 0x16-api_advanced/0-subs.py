f4acf7ca7da3ea7ce387ca40f294a3e604040fec
Matthew <122832898+Mat-t-hew@users.noreply.github.com>
2024-03-11 19:12:06 +0200

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
- function that queries the Reddit API
- and returns the number of subscribers
+function that queries the Reddit API
+and returns the number of subscribers
 """
 import requests
 
@@ -11,8 +11,24 @@ def number_of_subscribers(subreddit):
     if (type(subreddit) is not str):
         return(0)
     url_api = ("https://www.reddit.com/r/{}/about.json".format(subreddit))
-    headers = {'user-agent': 'safari:holberton/0.1.0'}
-    response = requests.get(url_api, headers=headers)
-    if response.status_code is not 200:
+    headers = {'User-Agent': 'python3:holberton'}
+    response = requests.get(url_api, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
         return(0)
-    return(response.json().get("data").get("subscribers"))
+    data = response.json().get("data", {}).get("subscribers", 0)
+    return data
+
+
+def validate_output(subreddit, expected_output):
+    """Validate the output"""
+    result = number_of_subscribers(subreddit)
+    if result == expected_output:
+        print("OK")
+    else:
+        print("Failed")
+
+
+if __name__ == "__main__":
+    # Test cases
+    validate_output("existingsubreddit", 0)  # Existing subreddit
+    validate_output("nonexistingsubreddit", 0)  # Non-existing subreddit
