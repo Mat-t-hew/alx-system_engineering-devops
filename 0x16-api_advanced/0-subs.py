747875730b960f6d82eff25e1f2de282a892af80
Matthew <122832898+Mat-t-hew@users.noreply.github.com>
2024-03-11 19:57:51 +0200

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,35 @@
 #!/usr/bin/python3
 """
- function that queries the Reddit API
- and returns the number of subscribers
+Function that queries the Reddit API and returns the number of subscribers.
 """
 import requests
 
-
 def number_of_subscribers(subreddit):
-    """initializate"""
-    if (type(subreddit) is not str):
-        return(0)
-    url_api = ("https://www.reddit.com/r/{}/about.json".format(subreddit))
+    """Initialize"""
+    if not isinstance(subreddit, str):
+        print("Invalid subreddit:", subreddit)
+        return 0
+
+    url_api = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {'user-agent': 'safari:holberton/0.1.0'}
+    
+    print("Request URL:", url_api)
     response = requests.get(url_api, headers=headers)
+    print("Response status code:", response.status_code)
+
     if response.status_code != 200:
-        return(0)
-    return(response.json().get("data").get("subscribers"))
+        print("Failed to get data from API")
+        return 0
+    
+    data = response.json().get("data")
+    if data is None:
+        print("No data received from API")
+        return 0
+
+    subscribers = data.get("subscribers")
+    print("Number of subscribers:", subscribers)
+    return subscribers
+
+# Test the function
+subreddit = "learnpython"
+print("The number of subscribers in r/{} is: {}".format(subreddit, number_of_subscribers(subreddit)))
