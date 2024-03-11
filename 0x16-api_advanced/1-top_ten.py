2eda24c6cf878ef630ba7f03bae83ebb4c85bf67
Matthew <122832898+Mat-t-hew@users.noreply.github.com>
2024-03-11 21:55:58 +0200

    Update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,23 +1,26 @@
 #!/usr/bin/python3
 """
-Queries the Reddit API and prints the first ten hot posts for a given subreddit
+Queries the Reddit API and prints the titles of the first ten hot posts
+for a given subreddit
 """
 import requests
 
 
 def top_ten(subreddit):
-    posts = ""
-    if (type(subreddit) is not str):
-        posts = "None\n"
-    else:
-        api_url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-        headers = {'user-agent': 'safari:schoolalx/0.7.4'}
-        response = requests.get(api_url, headers=headers)
-        if response.status_code is not 200:
-            posts = "None\n"
-        else:
-            posts_json = response.json().get("data").get("children")
-            for i in range(10):
-                post_title = posts_json[i].get("data").get("title")
-                posts += "{}\n".format(post_title)
-    print(posts, end="")
+    if not isinstance(subreddit, str):
+        print("None")
+        return
+    
+    url_api = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {'user-agent': 'safari:schoolalx/0.7.4'}
+    response = requests.get(url_api, headers=headers)
+    
+    if response.status_code != 200:
+        print("None")
+        return
+    
+    posts_json = response.json().get("data", {}).get("children", [])
+    
+    for i in range(min(10, len(posts_json))):
+        post_title = posts_json[i].get("data", {}).get("title", "")
+        print(post_title)
