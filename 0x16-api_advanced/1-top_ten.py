c3ac8a4630cee2db632eb9506dfab369068616cf
Matthew <122832898+Mat-t-hew@users.noreply.github.com>
2024-03-11 21:58:36 +0200

    Update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -10,17 +10,17 @@ def top_ten(subreddit):
     if not isinstance(subreddit, str):
         print("None")
         return
-    
+
     url_api = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {'user-agent': 'safari:schoolalx/0.7.4'}
     response = requests.get(url_api, headers=headers)
-    
+
     if response.status_code != 200:
         print("None")
         return
-    
+
     posts_json = response.json().get("data", {}).get("children", [])
-    
+
     for i in range(min(10, len(posts_json))):
         post_title = posts_json[i].get("data", {}).get("title", "")
         print(post_title)
